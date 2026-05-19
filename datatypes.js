c947e8290ddd35cf136461c4cf68fa3cc315b7d5
TUSHAR KADAM MALBA <tusharmalba@gmail.com>
2026-05-19 18:30:12 +0000

    datatypes file added

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+let int = 5;
+let float = 3.14;
+let string = "Hello, World!";
+let boolean = true;
+let nullValue = null;//null is also object type in JavaScript, but it represents the intentional absence of any object value. It is not the same as undefined, which means a variable has been declared but has not yet been assigned a value.
+let undefinedValue;
+
+console.log("Integer:", int);
+console.log("Float:", float);
+console.log("String:", string);
+console.log("Boolean:", boolean);
+console.log("Null Value:", nullValue);// This will print "Null Value: null" because null is a valid value in JavaScript and it represents the intentional absence of any object value. It is not the same as undefined, which means a variable has been declared but has not yet been assigned a value. 
+console.log("Undefined Value:", undefinedValue);
+
--- test.js
+++ test.js
@@ -2,9 +2,12 @@ console.log("hello")
 const accountid = 1234567890
 let accountEmail= "user@example.com"
 var accountName = "John Doe"
+let accountState ; // This variable is declared but not initialized, it will be undefined
 console.log("Account ID:", accountid)
 console.log("Account Email:", accountEmail)
 console.log("Account Name:", accountName)
 const accountPhone = "123-456-7890"
+
+// accountPhone = "123-456-7890" // This will cause an error because accountPhone is a constant
 console.log("Account Phone:", accountPhone)
-console.table({accountid, accountEmail, accountName, accountPhone})
+console.table({accountid, accountEmail, accountName, accountPhone, accountState})
