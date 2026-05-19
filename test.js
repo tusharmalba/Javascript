c53b2c2020ade528364b84c7b7d47fdb4af351d3
TUSHAR KADAM MALBA <tusharmalba@gmail.com>
2026-05-19 18:12:04 +0000

    basic js

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 console.log("hello")
+const accountid = 1234567890
+let accountEmail= "user@example.com"
+var accountName = "John Doe"
+console.log("Account ID:", accountid)
+console.log("Account Email:", accountEmail)
+console.log("Account Name:", accountName)
+const accountPhone = "123-456-7890"
+console.log("Account Phone:", accountPhone)
+console.table({accountid, accountEmail, accountName, accountPhone})
